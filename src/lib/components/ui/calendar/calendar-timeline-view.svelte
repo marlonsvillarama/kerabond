b9bc5bd68225455b081051a7ec5f939bb9e782fd
Marlon Villarama <marlonsvillarama@gmail.com>
2026-06-18 11:51:13 +1200

    20260618

--- src/lib/components/ui/calendar/calendar-timeline-view.svelte
+++ src/lib/components/ui/calendar/calendar-timeline-view.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { Check, Eye, EyeClosed } from "@lucide/svelte";
     const interval = 15;
     const startHour = 8;
     const endHour = 21;
@@ -23,28 +24,82 @@
         return output;
     });
 
-    let timelineStaff = [
+    const sortByKey = (list, key) => {
+        if (key) {
+            list.sort((a, b) => {
+                if (a[key] < b[key]) return -1;
+                if (b[key] < a[key]) return 1;
+                return 0;
+            });
+        }
+        else {
+            list.sort((a, b) => {
+                if (a < b) return -1;
+                if (b < a) return 1;
+                return 0;
+            });
+        }
+
+        list = list;
+        return list;
+    };
+
+    let allStaff = $state([
         { id: 1, name: 'John', initials: 'JF' },
         { id: 2, name: 'Apple', initials: 'AV' },
-        { id: 3, name: 'Marlong', initials: 'MV' },
-    ];
+        { id: 3, name: 'Marlong2', initials: 'MV' },
+        { id: 4, name: 'Marlong', initials: 'MV' },
+    ]);
+    allStaff = allStaff.map(d => {
+        return {
+            ...d, checked: true
+        };
+    })
+    allStaff = sortByKey(allStaff, 'name');
 
-    // let timelineHeaders = $state();
-    // let timelineHeaders = document.getElementById('timelineHeaders');
-    // console.log('timelineHeaders', timelineHeaders);
-    // timelineHeaders.style.setProperty('--staff-columns', timelineStaff.length);
+    let timelineStaff = $derived(sortByKey(allStaff.filter(d => d.checked === true), 'name'));
+
+    const updateTimelineStaff = (id) => {
+        let staff = allStaff.find(d => d.id === id);
+        let index = timelineStaff.findIndex(d => d.id === id);
+
+        if (index >= 0) {
+            timelineStaff.splice(index, 1);
+            timelineStaff = sortByKey(timelineStaff, 'name');
+            return;
+        }
+
+        timelineStaff.push(staff);
+        timelineStaff = sortByKey(timelineStaff, 'name');
+    }
 </script>
 
 <div class="fl-cal-timeline">
     <!-- <Sidebar root="/setup" items={sidebarItems} onselect={updateSetup} /> -->
     <div class="fl-sidebar">
-        timeline sidebar
+        <div class="fl-staff-list">
+            {#each allStaff as staff}
+            <div class="fl-staff-toggle">
+                <input type="checkbox" name="staff-{staff.id}" id="staff-{staff.id}"
+                    bind:checked={staff.checked}
+                    onchange={() => updateTimelineStaff(staff.id)}>
+                <label for="staff-{staff.id}" class="flex-center between">
+                    <span>{staff.name}</span>
+                    {#if staff.checked}
+                    <Eye size={16} />
+                    {:else}
+                    <EyeClosed size={16} />
+                    {/if}
+                </label>
+            </div>
+            {/each}
+        </div>
     </div>
 
     <div class="fl-content fl-page">
         <div class="fl-timeline-header">
             <div class="fl-timeline-corner"></div>
-            <div class="fl-timeline-staff">
+            <div class="fl-timeline-staff" style="grid-template-columns: repeat({timelineStaff.length}, 1fr);">
                 {#each timelineStaff as staff}
                     <div class="fl-timeline-staff-header">
                         <div class="fl-staff-avatar">{staff.initials}</div>
@@ -62,7 +117,7 @@
                         <span>{slot.slot}</span>
                         {/if}
                     </div>
-                    <div class="fl-timeline-slot" class:fl-slot-hour-start={slot.hourStart === true}>
+                    <div class="fl-timeline-slot" class:fl-slot-hour-start={slot.hourStart === true} style="grid-template-columns: repeat({timelineStaff.length}, 1fr);">
                         {#each timelineStaff as staff}
                             <div class="fl-staff-slot"></div>
                         {/each}
@@ -142,9 +197,7 @@
         flex: 1;
         border-top: 1px solid var(--border-light);
         height: 1.5rem;
-        --staff-columns: 3;
         display: grid;
-        grid-template-columns: repeat(var(--staff-columns), 1fr);
     }
     .fl-timeline-row.fl-slot-hour-start {
         border-top: 1px solid var(--accent-border);
@@ -153,6 +206,39 @@
         border-top: none;
     }
     .fl-staff-slot {
+        /* background-color: red; */
         border-left: 1px solid var(--accent-border);
+    }
+    .fl-staff-list {
+        /* border: 1px solid red; */
+        display: grid;
+        gap: 0.5rem;
+    }
+    .fl-staff-toggle {
+        position: relative;
+        /* border: 1px solid red; */
+    }
+    .fl-staff-toggle input[type=checkbox] {
+        position: absolute;
+        top: -9999px;
+        left: -9999px;
+    }
+    .fl-staff-toggle label {
+        padding: 0.5rem 0.75rem;
+        background-color: var(--border-light);
+        /* border: 1.5px solid var(--border-light); */
+        border-radius: 0.25rem;
+        cursor: pointer;
+        font-size: 0.875rem;
+        transition: all 100ms ease-in-out;
+    }
+    .fl-staff-toggle label:hover {
+        /* background-color: var(--accent-border); */
+        /* border: 1.5px solid var(--accent-border); */
+        box-shadow: var(--shadow);
+    }
+    .fl-staff-toggle input[type=checkbox]:checked + label {
+        background-color: var(--accent-border);
+        /* border-color: var(--accent); */
     }
 </style>
