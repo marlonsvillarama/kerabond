c41940fef4e3d8f0425373945b003f4fdb09162b
Marlon Villarama <marlonsvillarama@gmail.com>
2026-07-10 23:38:20 +1200

    20260710

--- src/routes/setup/services/service-card.svelte
+++ src/routes/setup/services/service-card.svelte
@@ -1,5 +1,6 @@
 <script>
     import { getContext } from "svelte";
+    import { Pencil, Plus, Trash } from "@lucide/svelte";
     import ServiceVariant from "./service-variant.svelte";
 
     let {
@@ -24,34 +25,34 @@
 <div class="fl-service-card">
     <div class="service-img"></div>
     <div class="service-details">
-        <div>
-        <div class="primary">
-            <div class="title">{data.name}</div>
-            <p class="desc">{data.description}</p>
-        </div>
-        <div class="controls">
-            <!-- <div class="title">&nbsp;</div> -->
-            controls
-        </div>
-        </div>
-
-        <div>
-        <div class="variants">
-            <div class="title">&nbsp;</div>
-            <div class="section-title title">
-                <span class="section-header small">Variants</span>
-                <div class="section-actions">
-                    <button type="button">Delete</button>
+        <div class="card-title">{data.name}</div>
+        <div class="card-details">
+            <div class="primary">
+                <div class="controls">
+                    <!-- <div class="title">&nbsp;</div> -->
+                    <!-- controls -->
+                    <button type="button"><Pencil size={20} />Edit</button>
+                    <button type="button"><Trash size={20} />Delete</button>
+                </div>
+                <p class="desc">{data.description}</p>
+                <div class="variants">
+                    <div class="section-title">
+                        <span>Variants</span>
+                        <!-- <span class="section-header small">Variants</span> -->
+                        <div class="section-actions">
+                            <button type="button">Add variant</button>
+                        </div>
+                    </div>
+                    {#each serviceVariants as _, i}
+                        <ServiceVariant bind:data={serviceVariants[i]} />
+                    {/each}
                 </div>
             </div>
-            {#each serviceVariants as _, i}
-                <ServiceVariant bind:data={serviceVariants[i]} />
-            {/each}
-        </div>
-        <div class="staff">
-            <div class="title">&nbsp;</div>
-            staff
-        </div>
+            <div class="secondary">
+                <div class="staff">
+                    staff
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -81,32 +82,97 @@
     }
     .fl-service-card > .service-details {
         padding: 1rem 1.25rem;
-        display: grid;
-        grid-template-columns: 3fr 2fr;
-        gap: 1.5rem;
+        position: relative;
     }
-    /* .fl-service-card > .service-details > * {
-        border: 1px solid red;
-    } */
-    .section-title {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-    .section-title.title {
+    .service-details > .card-title {
         font-size: 1.375rem;
         font-weight: 600;
         margin-bottom: 0.5rem;
     }
-    .section-header.small {
-        font-size: 1rem;
+    .service-details > .card-details {
+        display: grid;
+        grid-template-columns: 3fr 2fr;
+        gap: 1.5rem;
     }
-    .section-actions {}
-    .fl-service-card > .service-details .desc {
+    .card-details > .primary {
+        /* border: 1px solid red; */
+        display: flex;
+        flex-direction: column;
+        /* gap: 1rem; */
+    }
+    .primary > .controls {
+    /* .service-details > .controls { */
+        /* border: 1px solid red; */
+        /* position: absolute;
+        top: 0;
+        right: 0; */
+        display: flex;
+        align-items: center;
+        /* gap: 1px; */
+        gap: 0.25rem;
+        margin-bottom: 1rem;
+    }
+    .primary > .controls > button {
+    /* .service-details > .controls > button { */
+        /* background-color: transparent; */
+        background-color: var(--primary-lighter);
+        border: 0;
+        border-radius: 0.25rem;
+        /* border-bottom: 1px solid transparent; */
+        color: var(--primary);
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        gap: 0.375rem;
+        padding: 0.375rem 0.5rem;
+    }
+    /* .service-details > .controls > button:last-child {
+        border-top-right-radius: 0.5rem;
+    } */
+    .primary > .controls > button:hover {
+    /* .service-details > .controls > button:hover { */
+        background-color: var(--primary-light);
+        /* border-bottom: 1px solid var(--primary); */
+    }
+    .primary > .desc {
         color: var(--semi-dark);
         font-size: 0.875rem;
         line-height: 1.375rem;
         font-weight: 400;
         opacity: 0.8;
+        margin-bottom: 1rem;
     }
+    /* .fl-service-card > .service-details > * {
+        border: 1px solid red;
+    } */
+    .card-details .section-title {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        /* justify-content: space-between; */
+        font-size: 1rem;
+        font-weight: 500;
+        margin-bottom: 0.25rem;
+    }
+    .section-actions > button {
+        background-color: var(--primary);
+        border: 0;
+        border-radius: 0.25rem;
+        color: var(--white);
+        cursor: pointer;
+        font-size: 0.75rem;
+        outline: none;
+        padding: 0.25rem 0.625rem;
+    }
+    /* .section-header.small {
+        font-size: 1rem;
+    } */
+    /* .section-actions {} */
+    /* .fl-service-card > .service-details .desc {
+        color: var(--semi-dark);
+        font-size: 0.875rem;
+        line-height: 1.375rem;
+        font-weight: 400;
+        opacity: 0.8;
+    } */
 </style>
--- src/routes/setup/services/service-variant.svelte
+++ src/routes/setup/services/service-variant.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { Pencil, Trash } from "@lucide/svelte";
     let {
         data = $bindable()
     } = $props();
@@ -6,7 +7,12 @@
 
 <div class="fl-service-variant">
     <div class="name">
-        {data.name || `${data.duration} minutes`}
+        <span>{data.name || `${data.duration} minutes`}</span>
+        <span>${data.price}</span>
+    </div>
+    <div class="controls">
+        <button type="button"><Pencil size={16} /></button>
+        <button type="button"><Trash size={16} /></button>
     </div>
 </div>
 
@@ -17,8 +23,18 @@
         border: 1px solid var(--light);
         border-radius: 0.25rem;
         /* padding: 0.25rem; */
+        display: flex;
+        align-items: center;
+        gap: 1rem;
     }
     .fl-service-variant > .name {
         padding: 0.25rem 0.375rem;
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .fl-service-variant > .name > :first-child {
+        font-weight: 600;
     }
 </style>
